1f0f00fdfb6a0a53f70c112ffa3040f9c27bee00
Ludwig Ruderstaller <lr@cwd.at>
2015-09-25 11:44:14 +0200

    Unittests for importer - currently test against live crm

---
+++ src/Aspetos/Tests/Service/Vtiger/ImporterTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of Aspetos
+ *
+ * (c)2014 Ludwig Ruderstaller <lr@cwd.at>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Aspetos\Tests\Service\Vtiger;
+
+use Cwd\GenericBundle\Tests\Repository\DoctrineTestCase;
+
+/**
+ * Class Aspetos\UserTest
+ *
+ * @package Aspetos\Tests\Service
+ * @author  Ludwig Ruderstaller <lr@cwd.at>
+ */
+class ImporterTest extends DoctrineTestCase
+{
+    /**
+     * @var \Aspetos\Service\Vtiger\Importer
+     */
+    protected $service;
+
+    public function setUp()
+    {
+        $this->loadFixturesFromDirectory(__DIR__ . '/../DataFixtures');
+        $this->loginUser('admin', $this->getUser(1));
+        $this->service = $this->container->get('aspetos.service.vtiger.importer');
+    }
+
+    public function testGetClients()
+    {
+        $result = $this->service->getClients(1, 10);
+        $this->assertEquals(1, count($result));
+    }
+
+    protected function getUser($pid = 1)
+    {
+        return $this->container->get('aspetos.service.user')->find($pid);
+    }
+
+}
