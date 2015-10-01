9d675570b632fa66e45fd4965ac5c8949b3f2827
Ludwig Ruderstaller <lr@cwd.at>
2015-10-02 01:18:17 +0200

    [WIP] MediaBundle

---
+++ src/Cwd/MediaBundle/Service/MediaService.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * This file is part of aspetos.
+ *
+ * (c)2015 Ludwig Ruderstaller <lr@cwd.at>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Cwd\MediaBundle\Service;
+
+use Cwd\GenericBundle\Service\Generic;
+use Doctrine\ORM\EntityManager;
+use Monolog\Logger;
+
+/**
+ * Class MediaService
+ *
+ * @package CwdMediaBundel\Service
+ * @author  Ludwig Ruderstaller <lr@cwd.at>
+ */
+class MediaService extends Generic
+{
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @var bool
+     */
+    protected $debug = false;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(EntityManager $entityManager, Logger $logger, $config)
+    {
+        $this->config = $config;
+        $this->debug  = $config['throw_exception'];
+
+        parent::__construct($entityManager, $logger);
+
+        $this->directorySetup();
+    }
+
+    /**
+     * @param null|string $key
+     *
+     * @return string|array
+     */
+    public function getConfig($key = null)
+    {
+        if ($key !== null && isset($this->config[$key])) {
+            return $this->config[$key];
+        }
+
+        return $this->config;
+    }
+
+    public function storeImage($path) {
+        if (!file_exists($path) || !is_readable($path)) {
+            throw new \Exception('File does not exists or is not readable - '.$path);
+        }
+
+
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function directorySetup()
+    {
+        if (!is_dir($this->getConfig('storage')['path'])) {
+            mkdir($this->getConfig('storage')['path']);
+        } elseif (!is_writeable($this->getConfig('storage')['path'])) {
+            throw new \Exception('Storage Path not writeable');
+        }
+
+        if (!is_dir($this->getConfig('cache')['path'].'/'.$this->getConfig('cache')['dirname'])) {
+            mkdir($this->getConfig('cache')['path'].'/'.$this->getConfig('cache')['dirname']);
+        } elseif (!is_writeable($this->getConfig('cache')['path'].'/'.$this->getConfig('cache')['dirname'])) {
+            throw new \Exception('Cache Path not writeable');
+        }
+    }
+
+}
