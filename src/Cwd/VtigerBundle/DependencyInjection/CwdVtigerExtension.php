de0f031a0a111b42adcbd07946bf662a90f31eac
Ludwig Ruderstaller <lr@cwd.at>
2015-10-02 15:35:25 +0200

    lost services - rewrote to yaml

--- src/Cwd/VtigerBundle/DependencyInjection/CwdVtigerExtension.php
+++ src/Cwd/VtigerBundle/DependencyInjection/CwdVtigerExtension.php
@@ -23,7 +23,7 @@ class CwdVtigerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('cwd.vtiger.config', $config);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
