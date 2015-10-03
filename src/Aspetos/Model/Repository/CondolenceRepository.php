0fae5cb419b717dfb1391091ce89f814d45730f2
Niklas de Vries <niklas@mediajungle.at>
2015-10-03 13:04:55 +0200

    #3 added repository classes

---
+++ src/Aspetos/Model/Repository/CondolenceRepository.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of aspetos
+ *
+ * (c)2015 Ludwig Ruderstaller <lr@cwd.at>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Aspetos\Model\Repository;
+
+use Cwd\GenericBundle\Doctrine\EntityRepository;
+
+/**
+ * User Repository
+ *
+ * @author Ludwig Ruderstaller <lr@cwd.at>
+ * @SuppressWarnings("ShortVariable")
+ */
+class CondolenceRepository extends EntityRepository
+{
+}
