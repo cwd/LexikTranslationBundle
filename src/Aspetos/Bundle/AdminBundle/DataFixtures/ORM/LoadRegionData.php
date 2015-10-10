265c1c97ae0af10b7e4fe958893470c5100f2b06
Ludwig Ruderstaller <lr@cwd.at>
2015-10-11 01:16:22 +0200

    Adding fixture for regions

--- src/Aspetos/Bundle/AdminBundle/DataFixtures/ORM/LoadRegionData.php
+++ src/Aspetos/Bundle/AdminBundle/DataFixtures/ORM/LoadRegionData.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Aspetos\Tests\Service\DataFixtures;
+namespace Aspetos\Bundle\AdminBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
