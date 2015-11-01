554c74cfebc570e3323cf03f25d4f96d29615df0
Niklas de Vries <niklas.devries@cwd.at>
2015-11-01 04:34:14 -0800

    #21 master layout + demo index page

---
+++ src/Aspetos/Bundle/FrontendBundle/Resources/public/assets/candle/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by machw_000 on 05.10.2015.
+ */
+$(function() {
+
+    var candleSprite, flameSprite;
+
+    function initCandle(canvas,state) {
+        console.log(canvas,state);
+        var stage = new createjs.Stage($(canvas)[ 0 ]);
+        var candle = new createjs.Sprite(candleSprite);
+        candle.y = 55;
+        candle.gotoAndStop(state);
+        var flame = new createjs.Sprite(flameSprite);
+        stage.addChild(candle);
+        if(state <3) {
+            flame.gotoAndPlay(Math.floor(Math.random()*100));
+            flame.x = 4;
+            flame.y = 5 + state * 17;
+            stage.addChild(flame);
+        }
+
+        createjs.Ticker.addEventListener("tick", tick);
+        function tick(e) {
+            stage.update(e);
+        }
+    }
+
+    function init() {
+        var dataCandle = {
+            images: [preload.getResult('candle')],
+            frames: {width:90, height:120}
+        };
+        var dataFlame = {
+            images: [preload.getResult('animation')],
+            frames: {width:65, height:65},
+        };
+        candleSprite = new createjs.SpriteSheet(dataCandle);
+        flameSprite = new createjs.SpriteSheet(dataFlame);
+
+        $(".candle").each(function() {
+            var state = $(this).data('state');
+            var canvas = $(this).append("<canvas width='70' height='180'></canvas>").find('canvas');
+            initCandle(canvas,state);
+        });
+    }
+
+    var preload = new createjs.LoadQueue();
+    preload.addEventListener("complete", init);
+    preload.loadManifest([
+        {id: "candle", src:"/candle/img/candle_tiny.png"},
+        {id: "animation", src:"/candle/img/spritesheet_tiny.png"}
+    ]);
+});
