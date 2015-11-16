61c0026eb29ba4827561ff44c35ec73c3cb35fee
Ludwig Ruderstaller <lr@cwd.at>
2015-11-17 00:07:02 +0100

    refs #46

---
+++ src/Aspetos/Model/Entity/ReminderHistory.php
@@ -0,0 +1,83 @@
+<?php
+namespace Aspetos\Model\Entity;
+use Doctrine\ORM\Mapping AS ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Aspetos\Model\Repository\ReminderHistoryRepository")
+ */
+class ReminderHistory
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $result;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Aspetos\Model\Entity\Reminder", inversedBy="reminderHistories")
+     * @ORM\JoinColumn(name="reminderId", referencedColumnName="id", nullable=false)
+     */
+    private $reminder;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set result
+     *
+     * @param string $result
+     * @return ReminderHistory
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
+    /**
+     * Get result
+     *
+     * @return string 
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * Set reminder
+     *
+     * @param \Aspetos\Model\Entity\Reminder $reminder
+     * @return ReminderHistory
+     */
+    public function setReminder(\Aspetos\Model\Entity\Reminder $reminder)
+    {
+        $this->reminder = $reminder;
+
+        return $this;
+    }
+
+    /**
+     * Get reminder
+     *
+     * @return \Aspetos\Model\Entity\Reminder 
+     */
+    public function getReminder()
+    {
+        return $this->reminder;
+    }
+}
