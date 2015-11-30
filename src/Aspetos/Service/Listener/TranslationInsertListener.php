239358a145b07d9d651c3382b2a75ec400a8b572
Ludwig Ruderstaller <lr@cwd.at>
2015-11-30 07:19:17 +0100

    dont use listener in test env

--- src/Aspetos/Service/Listener/TranslationInsertListener.php
+++ src/Aspetos/Service/Listener/TranslationInsertListener.php
@@ -69,6 +69,10 @@ class TranslationInsertListener
             return;
         }
 
+        if (defined('TESTENV') && TESTENV) {
+            return;
+        }
+
         $messages = $this->translator->getCollectedMessages();
 
         foreach ($messages as $message) {
